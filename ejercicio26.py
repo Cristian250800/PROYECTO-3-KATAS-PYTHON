1ba8491dc3c1748c040e88d3bf243f209bc54d25
Cristian250800 <cristianmancha15@gmail.com>
2026-01-18 18:13:02 +0100

    Ejercicio 26

---
+++ ejercicio26.py
@@ -0,0 +1,7 @@
+'''Crea una función lambda que calcule el resto de la división entre dos números dados.'''
+
+def calcular_resto(a, b):
+    return a % b
+
+resultado = calcular_resto(17, 5)
+print(f"El resto es: {resultado}")
